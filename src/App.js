75d9f4db3497301e1aeca720f6ccffcb1942ef20
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-10-15 07:42:57 +0300

    add

--- src/App.js
+++ src/App.js
@@ -6,33 +6,29 @@ import About from "./components/About";
 import Users from "./components/Users";
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/users">Users</Link>
-              </li>
-            </ul>
-          </nav>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
 
-          {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/about" component={About} />
-            <Route path="/about" component={Users} />
-            <Route path="/about" component={Home} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/users" component={Users} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
